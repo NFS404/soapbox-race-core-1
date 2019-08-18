27e36e380f346edda932b9f4d7f06983feaea3a8
VladM (aka Hypercycle) <mvv0105nfs@gmail.com>
2019-08-18 21:17:26 +0700

    Chat announcer
    Online-count chat announcer (CHAT_ONLINECOUNT parameter on DB). Pushes a count message for all players every 10 minutes. 
    WARNING: very unstable and produces a Openfire timeouts for some reason. Should be redone.

--- src/main/java/com/soapboxrace/core/bo/OnlineUsersBO.java
+++ src/main/java/com/soapboxrace/core/bo/OnlineUsersBO.java
@@ -1,43 +1,59 @@
 package com.soapboxrace.core.bo;
 
-import java.util.Date;
+import java.util.List;
 
+import javax.ejb.AccessTimeout;
 import javax.ejb.EJB;
+import javax.ejb.Lock;
 import javax.ejb.Schedule;
-import javax.ejb.Stateless;
+import javax.ejb.Singleton;
+import javax.ejb.LockType;
 
-import com.soapboxrace.core.dao.OnlineUsersDAO;
-import com.soapboxrace.core.jpa.OnlineUsersEntity;
+import com.soapboxrace.core.bo.ParameterBO;
 import com.soapboxrace.core.xmpp.OpenFireRestApiCli;
+import com.soapboxrace.core.xmpp.OpenFireSoapBoxCli;
+import com.soapboxrace.core.xmpp.XmppChat;
 
-@Stateless
+import org.igniterealtime.restclient.entity.SessionEntity;
+
+@Singleton
 public class OnlineUsersBO {
 
 	@EJB
-	private OnlineUsersDAO onlineUsersDAO;
-
-	@EJB
 	OpenFireRestApiCli openFireRestApiCli;
-
+	
+	@EJB
+	private OpenFireSoapBoxCli openFireSoapBoxCli;
+	
+	private int onlineUsers;
+	
+	@EJB
+	private ParameterBO parameterBO;
+	
 	public int getNumberOfUsersOnlineNow() {
-		Date lastMinutes = getLastMinutes(1);
-		OnlineUsersEntity onlineUsersEntity = onlineUsersDAO.findByTime(lastMinutes);
-		return onlineUsersEntity != null ? onlineUsersEntity.getNumberOfUsers() : 0;
+		return onlineUsers;
 	}
-
+    
 	@Schedule(minute = "*", hour = "*", persistent = false)
-	public void insertNumberOfUsesOnlineNow() {
-		Long timeLong = new Date().getTime() / 1000L;
-		OnlineUsersEntity onlineUsersEntity = new OnlineUsersEntity();
-		onlineUsersEntity.setNumberOfUsers(openFireRestApiCli.getTotalOnlineUsers());
-		onlineUsersEntity.setTimeRecord(timeLong.intValue());
-		onlineUsersDAO.insert(onlineUsersEntity);
+	@Lock(LockType.READ)
+	public void updateOnlineUsers() {
+		onlineUsers = openFireRestApiCli.getTotalOnlineUsers();
 	}
-
-	private Date getLastMinutes(int minutes) {
-		long time = new Date().getTime();
-		time = time - (minutes * 90000);
-
-		return new Date(time);
+	
+	// OnlineCount Chat Announcer - Hypercycle
+	@Schedule(minute = "*/10", hour = "*", persistent = false)
+	@AccessTimeout(value=20000)
+	public void OnlineCountForChat() {
+		if (parameterBO.getBoolParam("CHAT_ONLINECOUNT")) {
+		  List<SessionEntity> allSessions = openFireRestApiCli.getAllSessions();
+		  String message = XmppChat.createSystemMessage("### Players Online: " + Integer.toString(openFireRestApiCli.getTotalOnlineUsers()));
+		    for (SessionEntity sessionEntity : allSessions) {
+		    String username = sessionEntity.getUsername();
+		      if (!username.contains("engine")) {
+		      Long member = Long.valueOf(username.replace("sbrw.", ""));
+		      openFireSoapBoxCli.send(message, member);
+		      }
+	        }
+		}
 	}
 }

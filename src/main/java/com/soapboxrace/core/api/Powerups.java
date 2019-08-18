ebb39f3793e104443f6667f15af4107b30b7ed75
VladM (aka Hypercycle) <mvv0105nfs@gmail.com>
2019-08-18 20:56:06 +0700

    Powerups on/off
    POWERUPS_ENABLED parameter (DB) allows to instantly switch the power-up functionality.

--- src/main/java/com/soapboxrace/core/api/Powerups.java
+++ src/main/java/com/soapboxrace/core/api/Powerups.java
@@ -9,10 +9,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import com.soapboxrace.core.api.util.Secured;
+import com.soapboxrace.core.bo.AchievementsBO;
 import com.soapboxrace.core.bo.InventoryBO;
 import com.soapboxrace.core.bo.ParameterBO;
+import com.soapboxrace.core.bo.PersonaBO;
 import com.soapboxrace.core.bo.TokenSessionBO;
+import com.soapboxrace.core.jpa.PersonaEntity;
 import com.soapboxrace.core.xmpp.OpenFireSoapBoxCli;
 import com.soapboxrace.jaxb.xmpp.XMPP_PowerupActivatedType;
 import com.soapboxrace.jaxb.xmpp.XMPP_ResponseTypePowerupActivated;
@@ -32,36 +34,44 @@ public class Powerups {
 	@EJB
 	private ParameterBO parameterBO;
 
+	@EJB
+	private AchievementsBO achievementsBO;
+
+	@EJB
+	private PersonaBO personaBO;
+
 	@POST
-	@Secured
 	@Path("/activated/{powerupHash}")
 	@Produces(MediaType.APPLICATION_XML)
 	public String activated(@HeaderParam("securityToken") String securityToken, @PathParam(value = "powerupHash") Integer powerupHash,
 			@QueryParam("targetId") Long targetId, @QueryParam("receivers") String receivers, @QueryParam("eventSessionId") Long eventSessionId) {
 		Long activePersonaId = tokenBO.getActivePersonaId(securityToken);
 
+		if (parameterBO.getBoolParam("POWERUPS_ENABLED")) {
+			XMPP_ResponseTypePowerupActivated powerupActivatedResponse = new XMPP_ResponseTypePowerupActivated();
+			XMPP_PowerupActivatedType powerupActivated = new XMPP_PowerupActivatedType();
+			powerupActivated.setId(Long.valueOf(powerupHash));
+			powerupActivated.setTargetPersonaId(targetId);
+			powerupActivated.setPersonaId(activePersonaId);
+			powerupActivatedResponse.setPowerupActivated(powerupActivated);
+			openFireSoapBoxCli.send(powerupActivatedResponse, activePersonaId);
+			for (String receiver : receivers.split("-")) {
+				Long receiverPersonaId = Long.valueOf(receiver);
+				if (receiverPersonaId > 10 && !activePersonaId.equals(receiverPersonaId)) {
+					openFireSoapBoxCli.send(powerupActivatedResponse, receiverPersonaId);
+				}
+		    }
+		}
+
 		if (!inventoryBO.hasItem(activePersonaId, powerupHash)) {
-			System.out.println(String.format("Persona %d doesn't have powerup %d", activePersonaId, powerupHash));
+//			System.out.println(String.format("Persona %d doesn't have powerup %d", activePersonaId, powerupHash));
 			return "";
 		}
-
-		XMPP_ResponseTypePowerupActivated powerupActivatedResponse = new XMPP_ResponseTypePowerupActivated();
-		XMPP_PowerupActivatedType powerupActivated = new XMPP_PowerupActivatedType();
-		powerupActivated.setId(Long.valueOf(powerupHash));
-		powerupActivated.setTargetPersonaId(targetId);
-		powerupActivated.setPersonaId(activePersonaId);
-		powerupActivatedResponse.setPowerupActivated(powerupActivated);
-		for (String receiver : receivers.split("-")) {
-			Long receiverPersonaId = Long.valueOf(receiver);
-			if (receiverPersonaId > 10) {
-				openFireSoapBoxCli.send(powerupActivatedResponse, receiverPersonaId);
-			}
-		}
-
 		if (parameterBO.getBoolParam("ENABLE_POWERUP_DECREASE")) {
 			inventoryBO.decrementUsage(activePersonaId, powerupHash);
 		}
-
+		PersonaEntity personaEntity = personaBO.getPersonaById(activePersonaId);
+		achievementsBO.applyPowerupAchievement(personaEntity);
 		return "";
 	}
 }
